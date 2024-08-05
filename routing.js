2edad48ac0fc7da9ffaddfb5899a3f2265a39009
solchuuxx <natalistevens1@hotmail.com>
2024-08-05 01:36:49 -0300

    cambios

--- routing.js
+++ routing.js
@@ -11,13 +11,21 @@ const processRequest = (req, res) => {
     case 'GET':
       switch (url) {
         case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
-          return res.end('<h1>Mi página<h1/>')
-        case '/about': 
+          return res.end('<h1>404<h1/>')
+      }
+    case 'POST':
+      switch (url) {
+        
       }
   }
 }
 
+// 46.04
 const server = http.createServer(processRequest)
 
 server.listen(1234, () => {
