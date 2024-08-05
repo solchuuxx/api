d75a73329187cdaf10e53b4a717097a67d54c415
solchuuxx <natalistevens1@hotmail.com>
2024-08-05 01:34:33 -0300

    first commit

---
+++ routing.js
@@ -0,0 +1,25 @@
+// <h1> <h1/>
+
+const http = require('node:http')
+
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>Mi página<h1/>')
+        case '/about': 
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
